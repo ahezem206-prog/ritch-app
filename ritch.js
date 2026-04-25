419effd974aa8103977e8f1a4193a84ea633f36b
ahezem206-prog <ahezem206@gmail.com>
2026-04-25 22:52:37 +0300

    Update ritch.js

--- ritch.js
+++ ritch.js
@@ -1,3 +1,4 @@
-alert("Test 1 2 3");
-document.body.innerHTML="<h1 style='color:red;font-size:50px;text-align:center;margin-top:200px'>IT WORKS</h1>";
-document.body.style.backgroundColor="blue";
+setTimeout(function() {
+  document.body.innerHTML = "<h1 style='color:red;font-size:50px;text-align:center;margin-top:200px'>IT WORKS</h1>";
+  document.body.style.backgroundColor = "blue";
+}, 1000);
